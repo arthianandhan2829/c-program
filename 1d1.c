37548ee7258be367ae5478160c63e519f9b92c4e
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-02-21 06:49:46 -0500

    Add files via upload

---
+++ 1d1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    int first = -1,second = -1;
+    for(int i=0;i<n;i++){
+        if(arr[i] > first){
+           second = first;
+           first = arr[i];
+    }   
+    else if(arr[i] > second && arr[i] < first){
+        second = arr[i];
+    }
+    }
+    printf("%d",second);
+    return 0;
+}
