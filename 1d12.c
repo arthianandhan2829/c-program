9c3c6bcbf2ef1b93136734857935cd849f04106c
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-02-27 12:37:43 -0500

    Add files via upload

---
+++ 1d12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr(size)
+    for(int i=0;i<size;i++){
+        scanf("%d",arr[i]);
+    }
+      int sum = 0;
+    for(int j=0;j<size;j++){
+      sum = sum+arr;
+    }
+    return 0;
+}
