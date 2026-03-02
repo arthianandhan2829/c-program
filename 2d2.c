61248f290ea7eed7031ff8d76177d707c72a6259
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-02 05:42:32 -0500

    Add files via upload

---
+++ 2d2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int b,c;
+    scanf("%d %d",&b,&c);
+    int a[b][c];
+    int i,j;
+    for(int i=0;i<b;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int max1 = -99999,min1 = 99999;
+    int max2 = -99999,min2 = 99999;
+    for(j=0;j<b;j++){
+        
+    }
+}
