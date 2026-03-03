fbb0ac7c855cd06529f8c818425b53896e70d5a7
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-03 07:30:37 -0500

    Add files via upload

---
+++ 2d5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int b,c;
+    scanf("%d%d",&b,&c);
+    int a[50][50],sum[50];
+    for(int i=0;i<b;i++){
+        sum[i] = 0;
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+            sum[i] +=a[i][j];
+        }
+    }
+    int maxdiff = 0;
+    for(int i=0;i<b;i++){
+        for(int j=1;j+=)
+    }
+}
