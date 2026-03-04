01098b3f615cbaf60c4c915d86f2b2efe0cc860e
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-04 05:25:37 -0500

    Add files via upload

---
+++ funx2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int maxiumumtwonumbers(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a<b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+    return 0;
+}
+int main(){
+ maxiumumtwonumbers();
+ return 0;
+ }
