b307aadf731336a14f7197b6a98defe6df5fe4ee
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-06 05:59:21 -0500

    Add files via upload

---
+++ point6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+
+    char *start = str;
+    char *end = str;
+    while (*(end + 1) != '\0') end++;
+
+    int flag = 1;
+    while (start < end) {
+        if (*start != *end) {
+            flag = 0;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    if (flag) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
